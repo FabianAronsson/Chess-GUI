1bf5aabd07e97a7398c31b2310cac5afc3ad9221
FabianAronsson <fabian.aronsson@elev.ga.ntig.se>
2021-05-11 08:29:38 +0200

    Release.
    Complete.

--- Chess/Chess/obj/Debug/Chess_Content.g.i.cs
+++ Chess/Chess/obj/Debug/Chess_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("sound/check.wav")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("logo.ico")]
 
 
